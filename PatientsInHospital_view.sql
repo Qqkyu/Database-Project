685bb57b87d70c077b268672ad903cd503474d39
Michał Kuliński <44360754+Qqkyu@users.noreply.github.com>
2021-01-30 20:34:26 +0100

    Update PatientsInHospital_view.sql

--- PatientsInHospital_view.sql
+++ PatientsInHospital_view.sql
@@ -1,11 +1,8 @@
-USE COVID19;
+DROP VIEW IF EXISTS dbo.Patients_in_Hospital;
 GO
 
-DROP VIEW IF EXISTS [dbo].[Patients_in_Hospital];
-GO
-
-CREATE VIEW [Patients_in_Hospital] AS
-	SELECT H.Facility_Name , H.Address ,(SELECT Count(*) FROM Hospitalized_Patients HP
-	GROUP BY  HP.Facility_Name
-	HAVING HP.Facility_Name=H.Facility_Name) AS PATIENTS_PER_HOSPITAL  FROM  Hospitals H;
+CREATE VIEW Patients_in_Hospital AS
+	SELECT H.Facility_Name, H.Address, (SELECT Count(*) FROM Hospitalized_Patients HP
+	GROUP BY HP.Facility_Name
+	HAVING HP.Facility_Name = H.Facility_Name) AS Patients_Per_Hospital FROM Hospitals H;
 GO
