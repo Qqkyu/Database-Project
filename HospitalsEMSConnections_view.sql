4a591b9026bbc8eeca5c9a6542b091c749121dd8
Michał Kuliński <44360754+Qqkyu@users.noreply.github.com>
2021-01-30 20:17:48 +0100

    Update HospitalsEMSConnections_view.sql

--- HospitalsEMSConnections_view.sql
+++ HospitalsEMSConnections_view.sql
@@ -1,12 +1,9 @@
-USE COVID19;
+DROP VIEW IF EXISTS dbo.Hospital_EMS_Connections;
 GO
 
-DROP VIEW IF EXISTS [dbo].[Hospital_EMS_Connections];
-GO
-
-CREATE VIEW [Hospital_EMS_Connections] 
+CREATE VIEW Hospital_EMS_Connections
 AS
-	SELECT H.Facility_Name,H.City,E.EMS_Address AS EMS_Address From Hospitals H
-	INNER JOIN EMS_Hospitals E ON E.Hospital_Facility_Name=H.Facility_Name AND E.Hospital_City=H.City 
+	SELECT H.Facility_Name, H.City, E.EMS_Address AS EMS_Address From Hospitals H
+	INNER JOIN EMS_Hospitals E ON E.Hospital_Facility_Name = H.Facility_Name AND E.Hospital_City = H.City 
 	INNER JOIN EMS_Details D ON D.Address = E.EMS_Address AND D.City = E.EMS_City
 GO
