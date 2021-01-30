9f5ee513102d01fc3e29a0e1c4caef81513d2e78
Michał Kuliński <44360754+Qqkyu@users.noreply.github.com>
2021-01-30 21:05:44 +0100

    Update GetContactNumber_func.sql

--- GetContactNumber_func.sql
+++ GetContactNumber_func.sql
@@ -1,10 +1,9 @@
 DROP FUNCTION IF EXISTS [dbo].[GetContactNumber];
 GO
 
-CREATE FUNCTION GetContactNumber (@PESEL varchar)
+CREATE FUNCTION GetContactNumber(@PESEL varchar)
 RETURNS Varchar
 AS
--- Zwraca numer do Pacjenta zadanego Peselem
 BEGIN
 	DECLARE @TEL varchar;
 	SELECT @TEL = P.Telephone_number FROM People P WHERE @PESEL = P.PESEL
