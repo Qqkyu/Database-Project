2b6529e7e1a12493a0b0bf193bedd3c630d3365f
jakmalkowski <57493594+jakmalkowski@users.noreply.github.com>
2021-01-30 20:59:51 +0100

    Changed(and improved) HospitalsInRegion

--- HospitalsInRegion_view.sql
+++ HospitalsInRegion_view.sql
@@ -2,5 +2,7 @@ DROP VIEW IF EXISTS dbo.HospitalsInRegion;
 GO
 
 CREATE VIEW HospitalsInRegion AS
-	SELECT * FROM Hospitals H ORDER BY Region DESC;
+	SELECT H.Facility_Name , H.Address , HD.Available_Beds , Hd.Available_Respirators ,HD.Infections_Diseases_Hospital FROM Hospitals H
+	JOIN Hospital_Details HD ON HD.Facility_Name = H.Facility_Name 
+	ORDER BY H.Region DESC;
 GO
