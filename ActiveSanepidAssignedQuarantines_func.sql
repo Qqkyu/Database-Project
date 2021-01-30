d4130169c7e1cbe535d6af4ab2ca79a881ee278b
Michał Kuliński <44360754+Qqkyu@users.noreply.github.com>
2021-01-30 20:43:58 +0100

    Update ActiveSanepidAssignedQuarantines_func.sql

--- ActiveSanepidAssignedQuarantines_func.sql
+++ ActiveSanepidAssignedQuarantines_func.sql
@@ -1,13 +1,9 @@
-USE COVID19;
-GO
-
 DROP FUNCTION IF EXISTS [dbo].[ACTIVE_SANEPID_ASSIGNED_QUARANTINES];
 GO
 
 CREATE FUNCTION ACTIVE_SANEPID_ASSIGNED_QUARANTINES (@SNPD_A varchar,@SNPD_C varchar)
 RETURNS TABLE
 AS
--- Funkcja zwraca wszystkie bierz¹ce kwarantanny zarz¹dzone przez dan¹ jednostkê sanepidu
 RETURN(
 	SELECT QD.PESEL,QD.Start_Date,QD.End_Date FROM Sanitary_Epidemiological_Stations T
 	JOIN Quarantined_People QP ON T.Address=QP.Sanepid_Station_Address AND T.City=QP.Sanepid_Station_City
