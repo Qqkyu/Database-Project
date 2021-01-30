cfb4a9edc55aa725e285756f8931276a00c80008
jakmalkowski <57493594+jakmalkowski@users.noreply.github.com>
2021-01-30 21:38:35 +0100

    Update AutoQuarantine_trigger.sql

--- AutoQuarantine_trigger.sql
+++ AutoQuarantine_trigger.sql
@@ -13,5 +13,5 @@ BEGIN
 		INSERT INTO Quarantined_People
 		SELECT I.PESEL, P.Region , (SELECT TOP 1 SE.Address FROM Sanitary_Epidemiological_Stations ORDER BY NEWID()) FROM People P
 		JOIN inserted I ON P.PESEL=I.PESEL
-		JOIN Sanitary_Epidemiological_Stations SE ON SE.Subservient_Region=P.PESEL
+		JOIN Sanitary_Epidemiological_Stations SE ON SE.Subservient_Region=P.Region
 END
