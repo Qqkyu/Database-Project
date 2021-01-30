3ffc175c670bd570a98a010be0d3bb356a975e47
jakmalkowski <57493594+jakmalkowski@users.noreply.github.com>
2021-01-30 21:45:30 +0100

    Update MakeBedAvailable_trigger.sql

--- MakeBedAvailable_trigger.sql
+++ MakeBedAvailable_trigger.sql
@@ -6,6 +6,7 @@ ON Hospitalized_Patients
 AFTER DELETE
 AS
 BEGIN
+	BEGIN TRANSACTION
 	DECLARE @Fac_name AS varchar(30)
 	Declare @Fac_city AS varchar(30)
 	SELECT @Fac_name = [Facility_Name] FROM inserted
@@ -13,4 +14,5 @@ BEGIN
 	UPDATE Hospital_Details
 	SET Available_Beds=Available_Beds+1
 	WHERE Facility_Name=@Fac_city AND City=@Fac_city
+	COMMIT;
 END
